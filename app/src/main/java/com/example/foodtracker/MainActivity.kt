1403c7dbc98763156818dd0ce7bce293600eea31
Wiktor <wiktorkalinowski@gmail.com>
2022-08-01 14:09:36 +0200

    Added compose ui module and welcome screen

--- app/src/main/java/com/example/foodtracker/MainActivity.kt
+++ app/src/main/java/com/example/foodtracker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.foodtracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.foodtracker.onboarding_presentation.welcome.WelcomeScreen
 import com.example.foodtracker.ui.theme.CaloryTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CaloryTrackerTheme {
-
+                WelcomeScreen()
             }
         }
     }
---
+++ onboarding/onboarding_presentation/src/main/java/com/example/foodtracker/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,39 @@
+package com.example.foodtracker.onboarding_presentation.welcome
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.core.R.string
+import com.example.core_ui.LocalSpacing
+import com.example.foodtracker.onboarding_presentation.components.ActionButton
+
+@Preview
+@Composable
+fun WelcomeScreen() {
+    val spacing = LocalSpacing.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(spacing.spaceMedium),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = string.welcome_text),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h1
+        )
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        ActionButton(
+            text = stringResource(id = string.next),
+            onCLick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
+}
